9729fea8d6d96aa15fb0bfcbe6c497054b96a97f
Murat Oğulcan Dursun <mdursun@k1m32s05.42istanbul.com.tr>
2025-03-16 19:21:34 +0300

    save

--- game.c
+++ game.c
@@ -6,7 +6,7 @@
 /*   By: mdursun <mdursun@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 17:34:03 by mdursun           #+#    #+#             */
-/*   Updated: 2025/03/09 17:03:53 by mdursun          ###   ########.fr       */
+/*   Updated: 2025/03/16 19:20:48 by mdursun          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,41 +26,49 @@ int	create_trgb(int t, int r, int g, int b)
 	return (t << 24 | r << 16 | g << 8 | b);
 }
 
-void checkmap(int x, int y, char *path)
-{
-	char	**map;
-	char	*line;
-	int		colmn;
-	int		fd;
-	int		i;
 
-	i = 0;
-	colmn = 0;
-	fd = open(path, O_RDONLY);
+size_t	getcol(char *path)
+{
+	int i = 0;
+	int colmn = 0;
+	int fd = open(path, O_RDONLY);
 	if (fd == -1)
 		exit (1);
-	line = get_next_line(fd);
-	while (line[i])
-	{
-		if (line[i] != 49)
-			exit (1);
-		i++;
-	}
+	char *line = get_next_line(fd);
 	while (line)
 	{
 		line = get_next_line(fd);
 		colmn++;
 	}
+	return (colmn);
+}
+
+char **checkmap(char *path)
+{
+	char	**map;
+	char	*line;
+	size_t		colmn;
+	int		fd;
+	int		i;
+
+	colmn = getcol(path);
+	fd = open(path, O_RDONLY);
+	if (fd == -1)
+		exit (1);
 	if (colmn < 3 )
 		exit (1);
 	map = malloc (sizeof(char *) * colmn);
 	line = get_next_line(fd);
+	i = 0;
 	while (line)
 	{
-		*map = malloc (ft_strlen(line));
+		map[i] = malloc (ft_strlen(line) + 1);
 		ft_strncpy (*map, line, ft_strlen(line));
+		*((map[i]) + ft_strlen(line)) = 0;
+		ft_strtrim(map[i]," ");
 		line = get_next_line(fd);
 	}
+	return (map);
 }
 
 int	close1(int keysym, t_vars *vars)
@@ -93,6 +101,15 @@ int	main(int argc, char *argv[])
 
 	int a = 64;
 	int b = 64;
+	int i = 0;
+	int j = 0;
+	size_t z = 0;
+	size_t k = 0;
+	char **mapp;
+
+	mapp = checkmap("./map.ber");
+	z = 0;
+	k = 0;
 	vars.img.player.x = 600;
 	vars.img.player.y = 300;
 	(vars.mlx = mlx_init());
@@ -101,12 +118,14 @@ int	main(int argc, char *argv[])
 	vars.img.player.img = mlx_xpm_file_to_image(vars.mlx, "pacman.xpm", &a , &b);
 	vars.img.map.addr = mlx_get_data_addr(vars.img.map.img, &vars.img.map.bits_per_pixel, &vars.img.map.line_length, &vars.img.map.endian);
 	vars.img.player.addr = mlx_get_data_addr(vars.img.player.img, &vars.img.player.bits_per_pixel, &vars.img.player.line_length, &vars.img.player.endian);
-	for (int y = 0 ; y < 900;y++)
+	while (k < 900)
 	{
-			for (int x = 0 ; x < 680;x++)
-			{	
-				my_mlx_pixel_put(&vars.img.map, y + 400, x + 150, create_trgb(0,230,24,46));
+			while (z < 680)
+			{
+				my_mlx_pixel_put(&vars.img.map, k + 400, z + 150, create_trgb(0,230,24,46));
+				z++;
 			}
+			k++;
 	}
 	mlx_put_image_to_window(vars.mlx, vars.win, vars.img.map.img, 0, 0);
 	mlx_put_image_to_window(vars.mlx, vars.win, vars.img.player.img, vars.img.player.x, vars.img.player.y);
